93faf6dfc55bfbcf9f1aacd68b4c77a7281faaab
kamalankit <riasdragon999@gmail.com>
2025-06-30 16:28:50 +0530

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -12,4 +12,7 @@ config.resolver.assetExts.push('css');
 // Ensure proper source extensions
 config.resolver.sourceExts.push('js', 'jsx', 'json', 'ts', 'tsx', 'mjs');
 
+// Add support for .cjs files (sometimes needed for certain packages)
+config.resolver.sourceExts.push('cjs');
+
 module.exports = config;
