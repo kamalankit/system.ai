c9b4ee2806ce69d50e176aa3c83835aae248ad37
kamalankit <riasdragon999@gmail.com>
2025-06-30 15:23:25 +0530

    Updated metro.config.js

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Enable CSS support for web
-config.resolver.assetExts.push('css');
-
-module.exports = config;
